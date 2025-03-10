125206957653b6911d42d560574de60811b16e8c
Las <me@las.rs>
2025-03-10 23:15:59 +0000

    remove registerOutputs from drv goal

--- src/libstore/build/derivation-goal.hh
+++ src/libstore/build/derivation-goal.hh
@@ -256,12 +256,6 @@ struct DerivationGoal : public Goal
     HookReply tryBuildHook();
 
     /**
-     * Check that the derivation outputs all exist and register them
-     * as valid.
-     */
-    virtual SingleDrvOutputs registerOutputs();
-
-    /**
      * Open a log file and a pipe to it.
      */
     Path openLogFile();
