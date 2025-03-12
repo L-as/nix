36e18d41a2923a777b072499e6b5a038ec27eb6f
Las <me@las.rs>
2025-03-12 18:15:59 +0000

    Remove handleChildOutput and handleEOF from goal subclasses

--- src/libstore/build/substitution-goal.cc
+++ src/libstore/build/substitution-goal.cc
@@ -177,8 +177,7 @@ Goal::Co PathSubstitutionGoal::tryToRun(StorePath subPath, nix::ref<Store> sub, 
         if (i != storePath) /* ignore self-references */
             assert(worker.store.isValidPath(i));
 
-    worker.wakeUp(shared_from_this());
-    co_await Suspend{};
+    co_await yield();
 
     trace("trying to run");
 
@@ -186,8 +185,7 @@ Goal::Co PathSubstitutionGoal::tryToRun(StorePath subPath, nix::ref<Store> sub, 
        if maxSubstitutionJobs == 0, we still allow a substituter to run. This
        prevents infinite waiting. */
     while (worker.getNrSubstitutions() >= std::max(1U, (unsigned int) settings.maxSubstitutionJobs)) {
-        worker.waitForBuildSlot(shared_from_this());
-        co_await Suspend{};
+        co_await waitForBuildSlot();
     }
 
     auto maintainRunningSubstitutions = std::make_unique<MaintainCount<uint64_t>>(worker.runningSubstitutions);
@@ -220,20 +218,20 @@ Goal::Co PathSubstitutionGoal::tryToRun(StorePath subPath, nix::ref<Store> sub, 
         }
     });
 
-    worker.childStarted(shared_from_this(), {
+    co_await childStarted({
 #ifndef _WIN32
         outPipe.readSide.get()
 #else
         &outPipe
 #endif
-    }, true, false);
-
-    co_await Suspend{};
+    }, true, false, [](Descriptor fd, std::optional<std::string_view> data) {
+        if (!data) return true;
+        else return false;
+    });
 
     trace("substitute finished");
 
     thr.join();
-    worker.childTerminated(this);
 
     try {
         promise.get_future().get();
@@ -277,13 +275,6 @@ Goal::Co PathSubstitutionGoal::tryToRun(StorePath subPath, nix::ref<Store> sub, 
 
     co_return done(ecSuccess, BuildResult::Substituted);
 }
-
-
-void PathSubstitutionGoal::handleEOF(Descriptor fd)
-{
-    worker.wakeUp(shared_from_this());
-}
-
 
 void PathSubstitutionGoal::cleanup()
 {
